a986479fe159fead40d37a2bc7e667499329c12d
WoroudFouleh <124286310+WoroudFouleh@users.noreply.github.com>
2023-05-12 04:20:27 -0700

    Update Admin.java

--- src/main/java/my_package/Admin.java
+++ src/main/java/my_package/Admin.java
@@ -11,7 +11,7 @@ public class Admin {
 	@SuppressWarnings("unused")
 	private int type;
 	private String name;
-	protected static final List<Admin> AA = new ArrayList<>() ;
+	protected static final List<Admin> AA = new ArrayList<Admin>() ;
 	final Logger logger = Logger.getLogger(Admin.class.getName());
 	
 	
